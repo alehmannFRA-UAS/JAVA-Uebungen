4fde14dadd256278932380075dab116418b1f442
alehmannFRA-UAS <88931807+alehmannFRA-UAS@users.noreply.github.com>
2022-12-26 12:30:55 +0000

    add comments to pass checkstyle test

--- src/main/java/edu/fra/uas/FirstProgram.java
+++ src/main/java/edu/fra/uas/FirstProgram.java
@@ -1,12 +1,25 @@
 package edu.fra.uas;
 
+/**
+ * First programm class
+ */
 public class FirstProgram{
 
+    /**
+     * main method to start processing
+     * @param args none needed here
+     */
     public static void main (String[] args){
         // Ausgabe
         System.out.println("Hello World!");
     }
 
+    /**
+     * simple method for multiplication of two integer
+     * @param a first integer
+     * @param b second integer
+     * @return multilpied result of a * b
+     */
     public int multiply(int a, int b) {
         return a * b;
     }
