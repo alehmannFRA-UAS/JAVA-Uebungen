a41b59332bcc5dab4063d606e746fc728fd2db2b
alehmannFRA-UAS <88931807+alehmannFRA-UAS@users.noreply.github.com>
2022-12-27 18:18:16 +0000

    Deploying to javadoc from @ alehmannFRA-UAS/JAVA-Uebungen@090b07abc57f56fcd942107ae475e3fa79399aae 🚀

---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"edu.fra.uas","c":"FirstProgram","l":"FirstProgram()","u":"%3Cinit%3E()"},{"p":"edu.fra.uas","c":"FirstProgram","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"edu.fra.uas","c":"FirstProgram","l":"multiply(int, int)","u":"multiply(int,int)"}];updateSearchResults();
